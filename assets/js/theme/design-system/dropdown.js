1600d5497e17ca49716a5d81e023d57d257ac71a
Olivia206 <olivia.simonet@mmibordeaux.com>
2022-07-29 15:44:58 +0200

    Dropdown

---
+++ assets/js/theme/design-system/dropdown.js
@@ -0,0 +1,33 @@
+const CLASSES = {
+    DropdownOpened: 'is-opened'
+};
+
+class Dropdown {
+    constructor (selector) {
+        this.element = document.querySelector(selector);
+        this.dropdownButton = this.element.querySelector('button');
+
+        this.state = {
+            isOpened: false
+        };
+
+        this.listen();
+    }
+
+    listen () {
+        this.dropdownButton.addEventListener('click', () => {
+            this.toggleDropdown();
+        });
+    }
+
+    toggleDropdown (open = !this.state.isOpened) {
+        console.log('lalala')
+        let classAction;
+        this.state.isOpened = open;
+        classAction = this.state.isOpened ? 'add' : 'remove';
+        this.dropdownButton.setAttribute('aria-expanded', this.state.isOpened);
+        this.element.classList[classAction](CLASSES.DropdownOpened);
+    }
+}
+
+export default new Dropdown('.diplomas-select');
--- assets/js/theme/index.js
+++ assets/js/theme/index.js
@@ -1,6 +1,7 @@
 import './body.js';
 import './blocks/keyFigures';
 import './blocks/timeline';
+import './design-system/dropdown';
 import './nav/stickyNav';
 // import './nav/responsive';
 import './nav/mainMenu';
