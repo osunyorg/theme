f1ab7c3396806fff74b1a82f2adf9c1135a9bfd1
alexisben <alex@noesya.coop>
2022-11-16 18:19:09 +0100

    toc offcanvas wip

---
+++ assets/js/theme/design-system/toc.js
@@ -0,0 +1,47 @@
+
+const CLASSES = {
+  // TODO : refacto classnames et modifier le nom de la class pour "is-overlay-visible"
+  menusOpened: 'has-menu-opened',
+};
+
+class TableOfContent {
+  constructor() {
+    this.element = document.querySelector('.toc-container');
+    this.links = this.element.querySelectorAll('a');
+    this.togglers = document.querySelectorAll('.toc-cta, .toc-container button');
+    this.state = {
+      opened: false
+    }
+    this.listen();
+  }
+  listen() {
+    window.addEventListener('scroll', this.update.bind(this));
+
+    this.togglers.forEach(toggler => {
+      toggler.addEventListener('click', () => {
+        this.toggle();
+      });
+    })
+
+    window.addEventListener('click', (event) => {
+      if (event.target === document.body) {
+          this.toggle(false);
+      }
+    });
+  }
+  toggle(open) {
+    this.state.opened = typeof open !== 'undefined' ? open : !this.state.opened;
+
+    const classAction = this.state.opened ? 'add' : 'remove';
+    this.element.classList[classAction]('is-opened');
+    document.documentElement.classList[classAction](CLASSES.menusOpened);
+  }
+  update() {
+    // WIP
+    this.links.forEach(link => {
+
+    })
+  }
+}
+
+new TableOfContent()
