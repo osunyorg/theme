ba61f3b0d76c5c365537ad421a0bdee84cc5549b
alexisben <alex@noesya.coop>
2022-11-17 16:00:28 +0100

    sideboar scrollspy

--- assets/js/theme/design-system/toc.js
+++ assets/js/theme/design-system/toc.js
@@ -7,7 +7,9 @@ const CLASSES = {
 class TableOfContent {
   constructor() {
     this.element = document.querySelector('.toc-container');
+    this.content = this.element.querySelector('.toc-content');
     this.links = this.element.querySelectorAll('a');
+    this.sections = document.querySelectorAll('section');
     this.togglers = document.querySelectorAll('.toc-cta, .toc-container button');
     this.state = {
       opened: false
@@ -38,9 +40,27 @@ class TableOfContent {
   }
   update() {
     // WIP
-    this.links.forEach(link => {
+    const scroll = document.documentElement.scrollTop || document.body.scrollTop;
 
-    })
+    this.sections.forEach(section => {
+      if (section.offsetTop <= scroll - window.innerHeight / 2) {
+        this.activateLink(section.id);
+      }
+    });
+
+    this.updateSidebarScroll(scroll);
+  }
+  activateLink(id) {
+    const currentLink = this.element.querySelector(`[href*=${ id }]`);
+    this.links.forEach(link => link.classList.remove('active'));
+    if (currentLink) {
+      currentLink.classList.add('active');
+      // this.nav.scrollTo(0, currentLink.offsetTop - window.innerHeight/2);
+    }
+  }
+  updateSidebarScroll(scroll) {
+    const progression = Math.max((scroll - window.innerHeight) / document.body.offsetHeight, 0);
+    this.content.scrollTo(0, progression * window.innerHeight/2)
   }
 }
 
