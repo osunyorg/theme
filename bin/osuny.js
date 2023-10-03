bd81313fabbadb8053ebb2842cbf14f4b6154611
Arnaud Levy <contact@arnaudlevy.com>
2023-10-03 18:09:05 +0200

    build with params

--- bin/osuny.js
+++ bin/osuny.js
@@ -20,47 +20,43 @@ console.log(`
 const command = process.argv[2];
 
 if (command === "watch") {
-    console.log("watch");
-    shell.exec("hugo server")
+    shell.exec("hugo server");
 }
 
 if (command === "dev") {
-    console.log("dev");
     shell.exec("hugo");
-    shell.exec("npx pagefind --site public --output-subdir ../static/pagefind")
-    shell.exec("hugo server")
+    shell.exec("cd themes/osuny-hugo-theme-aaa");
+    shell.exec("npx pagefind --site ../../public --output-subdir ../static/pagefind");
+    shell.exec("cd ../..");
+    shell.exec("hugo server");
 }
 
 if (command === "build") {
-    console.log("build");
     shell.exec("hugo");
-    shell.exec("npm_config_yes=true npx pagefind --site public");
+    shell.exec("cd themes/osuny-hugo-theme-aaa");
+    shell.exec("npm_config_yes=true npx pagefind --site ../../public");
+    shell.exec("cd ../..");
 }
 
 if (command === "update") {
-    console.log("update");
     shell.exec("git pull --recurse-submodules --depth 1");
     shell.exec("git submodule update --remote");
 }
 
 if (command === "setup-example") {
-    console.log("setup-example");
     shell.exec("git submodule add https://github.com/noesya/osuny-example");
 }
 
 if (command === "server-example") {
-    console.log("server-example");
     shell.exec("hugo server --config osuny-example/config/example/config.yaml");
 }
 
 if (command === "example") {
-    console.log("example");
     shell.exec("yarn setup-example > /dev/null || yarn update");
     shell.exec("yarn server-example");
 }
 
 if (command === "update-theme") {
-    console.log("update-theme");
     shell.exec("cd themes/osuny-hugo-theme-aaa");
     shell.exec("git checkout main");
     shell.exec("git pull");
