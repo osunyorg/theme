a72a311328e244f83edd912d0ad32c0d40904052
Olivia206 <olivia.simonet206@gmail.com>
2024-02-21 15:09:20 +0100

    oups

--- assets/js/vendors/lightbox.js
+++ assets/js/vendors/lightbox.js
@@ -1,24 +1,6 @@
 import gLightbox from 'glightbox';
 
-let lightboxBtn;
-
-const lightbox = gLightbox({
+gLightbox({
     openEffect: 'fade',
-    closeEffect: 'fade',
-    onOpen: () => {
-        lightboxBtn = document.activeElement;
-    },
-    onClose: () => {
-        if (lightboxBtn) {
-            lightboxBtn.focus();
-        }
-    }
-});
-
-lightbox.on('slide_changed', () => {
-    const currentSlide = document.querySelector('.gslide.current');
-    if (currentSlide) {
-        currentSlide.setAttribute("tabindex", "0");
-        currentSlide.focus();
-    }
+    closeEffect: 'fade'
 });
