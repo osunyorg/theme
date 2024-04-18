03d63d517ab1329a0629d1322629eefc843fdb18
Olivia206 <olivia.simonet206@gmail.com>
2024-04-18 15:35:04 +0200

    cleaned focus trap js

--- assets/js/theme/utils/focus-trap.js
+++ assets/js/theme/utils/focus-trap.js
@@ -1,10 +1,9 @@
 export function focusTrap(event, element, isOpened) {
-    const focusables = 'a, button, input, textarea, select, details, [tabindex], [contenteditable="true"]';
-    const elements = element.querySelectorAll(focusables);
-    
-    const focusableInDialog = Array.from(elements).filter(element => element.tabIndex >= 0);
-    const firstFocusable = focusableInDialog[0];
-    const lastFocusable = focusableInDialog.at(-1);
+    const focusables = 'a, button, input, textarea, select, details, [tabindex], [contenteditable="true"]',
+    elements = element.querySelectorAll(focusables),
+    focusableInDialog = Array.from(elements).filter(element => element.tabIndex >= 0),
+    firstFocusable = focusableInDialog[0],
+    lastFocusable = focusableInDialog.at(-1);
 
     if (!isOpened) {
         return;
@@ -13,7 +12,7 @@ export function focusTrap(event, element, isOpened) {
         lastFocusable.focus();
         event.preventDefault();
     }
-    else if (!element.contains(event.target)) {            
+    else if (!element.contains(event.target)) {
         firstFocusable.focus();
         event.preventDefault();
     }
