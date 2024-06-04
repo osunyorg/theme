8377712c570ded955072c1ac42ba8cc96c7ce592
Olivia206 <olivia.simonet@mmibordeaux.com>
2024-06-04 09:31:34 +0200

    wording

--- assets/js/theme/blocks/keyFigures.js
+++ assets/js/theme/blocks/keyFigures.js
@@ -117,7 +117,7 @@ class KeyFigures {
 }
 
 window.addEventListener('load', () => {
-    const keyFigures = document.querySelectorAll('.block-key_figures.with-animation');
+    const keyFigures = document.querySelectorAll('.block-key_figures.is-animated');
     keyFigures.forEach((dom) => {
         new KeyFigures(dom);
     });
