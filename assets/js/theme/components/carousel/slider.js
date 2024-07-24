365541942c47c85e5b1f2d8d91d2da4780fde90c
Clara Rigaud <clararigaud0@gmail.com>
2024-07-24 14:36:58 +0200

    rearanged slider

--- assets/js/theme/components/carousel/slider.js
+++ assets/js/theme/components/carousel/slider.js
@@ -8,163 +8,143 @@ if (!window.osuny.carousel) {
 window.osuny.carousel.Slider = function Slider(instance) {
     this.instance = instance;
     this.index = 0;
+    this.offset = 0;
     this.slides = [];
+    this.deltaPosition = 0;
+    this.position = 0;
     this.initialize();
 }
 
 window.osuny.carousel.Slider.prototype = {
     initialize: function () {
-        
+
+        //cleaning clones ( if any)
+        var clones = this.instance.container.querySelectorAll(":scope > .clone");
+        for (var i = 0; i < clones.length; i += 1) {
+            this.instance.container.removeChild(clones[i]);
+        }
+
+        // Adding new clones
+        var domElement;
+        var childrenAsArray = Array.from(this.instance.container.children);
+        for (var i = 0; i < childrenAsArray.length; i++) {
+            domElement = childrenAsArray[i];
+            domElement.setAttribute("id", "slide__" + i);
+
+            var clone = domElement.cloneNode(true);
+            clone.classList.add("clone");
+            clone.setAttribute("id", "slide__clone__" + i);
+            this.instance.container.appendChild(clone); // ajout d'une copie a la fin
+        }
+
+        this.updateElements();
+        this.translate(-this.size(), 500);
+
+        var slideTranslationFinished = this.slideTranslationFinished.bind(this);
+
+        this.instance.container.addEventListener('transitionend', function (e) {
+            if (e.propertyName == "left") {
+                slideTranslationFinished();
+            }
+        });
     },
     nextSlide: function () {
-        
+        this.showSlide(this.index + 1);
     },
     previousSlide: function () {
-
+        this.showSlide(this.index - 1)
     },
     showSlide: function (index) {
+        var offset = index - this.index;
+        var delta = 0; // distance à parcourir
+        var sign = Math.sign(offset); // direction positive ou negative 
+        // on parcours le nombre de slide à déplacer
+        for (var i = 0; i < Math.abs(offset); i += 1) {
+            // on ajoute ( ou soustrait en fonction du signe), la taille de chacun des slide
+            delta -= sign * this.slideAt(offset - (sign > 0) - sign * i).size;
+        }
 
+        // On active l'animation
+        this.translate(delta, this.instance.options.transition_duration);
+
+        // pour l'instant on a pas bougé, on ajoute le decalage au décalage eventuel ( ou 0)
+        this.offset += offset;
+
+        // On met à jour le slide current 
+        this.index = this.indexOfSlideAt(offset);
+    },
+    translate: function (delta, transition = 0) {
+        this.deltaPosition = delta - this.deltaPosition;
+        this.position += this.deltaPosition;
+        this.deltaPosition = 0;
+        this.instance.container.style.setProperty('transition', 'left ' + String(transition) + 'ms');
+        this.instance.container.style.setProperty('left', this.position + "px");
+    },
+    slideTranslationFinished: function () {
+        if (this.offset > 0) {
+            for (var i = 0; i < this.offset; i++) {
+                this.putFirstASlideAtEnd();
+            }
+        } else if (this.offset < 0) {
+            for (var i = 0; i < Math.abs(this.offset); i++) {
+                this.putLastSlideAtbegining();
+            }
+        }
+        this.offset = 0;
+    },
+    putFirstASlideAtEnd: function () {
+        var container = this.instance.container;
+        var travellingSlide = container.children.item(0);
+        var travellingSlideId = parseInt(travellingSlide.getAttribute("id").slice(-1));
+        var travellingSlideWidth = this.slides[travellingSlideId].size;
+        this.translate(travellingSlideWidth);
+        container.append(container.removeChild(travellingSlide));
+    },
+    putLastSlideAtbegining: function () {
+        var container = this.instance.container;
+        var travellingSlide = container.children.item(container.children.length - 1)
+        var travellingSlideId = parseInt(travellingSlide.getAttribute("id").slice(-1));
+        var travellingSlideWidth = -this.slides[travellingSlideId].size;
+        this.translate(travellingSlideWidth);
+        container.prepend(container.removeChild(travellingSlide));
+    },
+    slideAt: function (offset) {
+        var index = this.indexOfSlideAt(offset);
+        return this.slides[index];
+    },
+
+    indexOfSlideAt: function (offset) {
+        // returns the index of the slide located at the distance "offset" from current slide
+        var trackLen = this.slides.length;
+        return ((this.index + offset) % trackLen + trackLen) % trackLen;
+    },
+    updateElements: function () {
+        this.slides = [];
+        var elems = this.instance.container.querySelectorAll(":scope > :not(.clone)");
+        for (var i = 0; i < elems.length; i += 1) {
+            this.slides.push({
+                dom: elems[i],
+                size: this.computeElementWidth(elems[i])
+            });
+        }
+    },
+    size: function () {
+        var size = 0;
+        for (var i = 0; i < this.slides.length; i++) {
+            size += this.slides[i].size;
+        }
+        return size;
+    },
+    computeElementWidth: function (domElement) {
+        var style = getComputedStyle(domElement);
+        var width = domElement.offsetWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+        return width
+    },
+    computeElementsWidth: function (domElements) {
+        var width = 0;
+        for (var i = 0; i < domElements.length; i += 1) {
+            width += this.computeElementWidth(domElements[i])
+        }
+        return width;
     }
 }
-// this.domElement.addEventListener('transitionend', function (e) {
-//     if (e.propertyName == "left") {
-//         this.onTransitioned();
-//     }
-// });
-
-
-
-//     this.current = 0;
-//     this.position = 0;
-//     this.elements = [];
-//     this.delta = 0;
-
-//     //cleaning clones ( if any)
-//     var clones = this.domElement.querySelectorAll(":scope > .clone");
-//     for (var i = 0; i < clones.length; i += 1) {
-//         this.domElement.removeChild(clones[i]);
-//     }
-
-//     // Adding new clones
-//     var domElement;
-//     var childrenAsArray = Array.from(this.domElement.children);
-//     for (var i = 0; i < childrenAsArray.length; i++) {
-//         domElement = childrenAsArray[i];
-//         domElement.setAttribute("id", "slide__" + i);
-
-//         var clone = domElement.cloneNode(true);
-//         clone.classList.add("clone");
-//         clone.setAttribute("id", "slide__clone__" + i);
-//         this.domElement.appendChild(clone); // ajout d'une copie a la fin
-//     }
-
-//     this.updateElements();
-//     this.translate(-this.size(), 500);
-
-//     // var onTransitioned = this.onTransitioned.bind(this);
-// }
-
-// window.osuny.carousel.Slider.prototype.updateElements = function () {
-//     this.elements = [];
-//     var elems = this.domElement.querySelectorAll(":scope > :not(.clone)");
-//     for (var i = 0; i < elems.length; i += 1) {
-//         this.elements.push({
-//             dom: elems[i],
-//             size: this.computeElementWidth(elems[i])
-//         });
-//     }
-// };
-
-// window.osuny.carousel.Slider.prototype.translate = function (delta, transition = 0) {
-//     // on enregistre également la distance à parcourir
-//     // si le delta n'était pas à 0 on le soustrait à la distance à parcourir
-//     this.delta = delta - this.delta;
-//     this.position += this.delta;
-//     this.delta = 0;
-//     this.domElement.style.setProperty('transition', 'left ' + String(transition) + 'ms');
-//     this.domElement.style.setProperty('left', this.position + "px");
-// }
-
-// window.osuny.carousel.Slider.prototype.putFirstAtEnd = function () {
-//     var traveller = this.domElement.children.item(0);
-//     var travellerId = parseInt(traveller.getAttribute("id").slice(-1));
-//     var width = this.element(travellerId).size;
-//     this.translate(width);
-//     this.domElement.append(this.domElement.removeChild(traveller));
-// };
-
-// window.osuny.carousel.Slider.prototype.putLastAtbegining = function () {
-//     var traveller = this.domElement.children.item(this.domElement.children.length - 1)
-//     var travellerId = parseInt(traveller.getAttribute("id").slice(-1));
-//     var width = -this.element(travellerId).size;
-//     this.translate(width);
-//     this.domElement.prepend(this.domElement.removeChild(traveller));
-// };
-
-// // ACCESSORS
-// window.osuny.carousel.Slider.prototype.currentSlide = function () {
-//     return this.current;
-// }
-
-// window.osuny.carousel.Slider.prototype.getPosition = function () {
-//     return this.position;
-// };
-
-// window.osuny.carousel.Slider.prototype.element = function (index) { //element {dom, size} (hors clone à l'index index)
-//     var elements = this.elements;
-//     return elements[index];
-// };
-
-// window.osuny.carousel.Slider.prototype.elementAt = function (offset) {
-//     var index = this.numElementAt(offset);
-//     return this.elements[index];
-// };
-
-// window.osuny.carousel.Slider.prototype.size = function () {
-//     var size = 0;
-//     for (var i = 0; i < this.elements.length; i++) {
-//         size += this.elements[i].size;
-//     }
-//     return size;
-// };
-
-// window.osuny.carousel.Slider.prototype.computeElementWidth = function (dom) {
-//     var style = getComputedStyle(dom);
-//     var width = dom.offsetWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight);
-//     return width
-// };
-
-// window.osuny.carousel.Slider.prototype.computeElementsWidth = function (domElems) {
-//     var width = 0;
-//     for (var i = 0; i < domElems.length; i += 1) {
-//         width += this.computeElementWidth(domElems[i])
-//     }
-//     return width;
-// };
-
-// window.osuny.carousel.Slider.prototype.updateCurrent = function (offset) {
-//     this.current = this.numElementAt(offset);
-// };
-
-// window.osuny.carousel.Slider.prototype.numElementAt = function (offset) {
-//     // returns the index of the slide located at the distance "offset" from current slide
-//     var trackLen = this.elements.length;
-//     return ((this.current + offset) % trackLen + trackLen) % trackLen;
-// };
-
-// window.osuny.carousel.Slider.prototype.updateDom = function () {
-//     /// WIP
-//     var currentPosition = 0;
-//     var firstSlideNumber = parseInt(this.domElement.children.item(0).getAttribute("id").slice(-1));
-//     var delta = this.position + this.size(); // delta centré doit être 0
-//     var direction = Math.sign(delta);
-//     var offsetValue = firstSlideNumber - this.current;
-//     console.log("current:", this.current, "deltaPos:", delta, "offset:", offsetValue);
-
-//     var nOffset = delta % this.size();
-//     // console.log(nOffset);
-//     // for(var i = 0; i <= this.domElement.children.length; i += 1){
-
-//     // }
-// };
-
