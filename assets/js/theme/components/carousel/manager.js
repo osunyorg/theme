f53f93837feca31247bd7d78752dc22f757caa20
Clara Rigaud <clararigaud0@gmail.com>
2024-09-16 12:06:51 +0200

    eslint + adding id to carousel

--- assets/js/theme/components/carousel/manager.js
+++ assets/js/theme/components/carousel/manager.js
@@ -18,24 +18,28 @@ window.osuny.carousel.manager = {
         }
     },
     _createCarousels: function () {
+        var element,
+            carousel,
+            i;
         this.elements = document.getElementsByClassName(window.osuny.carousel.classes.carousel);
-        for (var i = 0; i < this.elements.length; i += 1) {
-            var element = this.elements[i],
-                carousel = new window.osuny.carousel.Carousel(element);
+        for (i = 0; i < this.elements.length; i += 1) {
+            element = this.elements[i];
+            element.setAttribute('id', window.osuny.carousel.classes.carousel + '-' + i);
+            carousel = new window.osuny.carousel.Carousel(element);
             this.carousels.push(carousel);
         }
     },
     _initializeListeners: function () {
         window.addEventListener(
-            "resize",
+            'resize',
             this._resize.bind(this)
         );
         window.addEventListener(
-            "scroll",
+            'scroll',
             this._findCarouselsInViewport.bind(this)
         );
         window.addEventListener(
-            "keydown",
+            'keydown',
             this._onKeyPress.bind(this)
         );
     },
@@ -45,51 +49,58 @@ window.osuny.carousel.manager = {
             carousel.resize();
         });
     },
-    _computeWindowCenterY: function(){
+    _computeWindowCenterY: function () {
         this.windowCenterY = (window.innerHeight || document.documentElement.clientHeight) / 2;
     },
     _findCarouselsInViewport: function () {
+        var i = 0,
+            carousel = null;
         this.carouselsInViewport = [];
-        for (var i = 0; i < this.carousels.length; i += 1) {
-            var carousel = this.carousels[i];
+        for (i = 0; i < this.carousels.length; i += 1) {
+            carousel = this.carousels[i];
             if (carousel.isInViewPort()) {
                 carousel.unpause();
                 this.carouselsInViewport.push(carousel);
             } else {
                 carousel.pause();
             }
-        };
+        }
         this.focusedCarousel = this._findBestCarouselFocusCandidate();
     },
     _findBestCarouselFocusCandidate: function () {
         // On démarre avec la plus grande distance possible
         var distance = window.innerHeight,
-            bestCandidate = null;
-        for (var i = 0; i < this.carousels.length; i += 1) {
-            var carousel = this.carousels[i];
-            var currentDistanceToCenter = Math.abs(carousel.getCenterPositionY() - this.windowCenterY);
+            bestCandidate = null,
+            i = 0,
+            carousel,
+            currentDistanceToCenter;
+        for (i = 0; i < this.carousels.length; i += 1) {
+            carousel = this.carousels[i];
+            currentDistanceToCenter = Math.abs(carousel.getCenterPositionY() - this.windowCenterY);
             if (currentDistanceToCenter < distance) {
                 distance = currentDistanceToCenter;
                 bestCandidate = carousel;
             }
-        };
+        }
         return bestCandidate;
     },
     _onKeyPress: function (e) {
         if (this.focusedCarousel) {
-            if (e.key == 'ArrowLeft') { this.focusedCarousel.previous() }
-            else if (e.key == 'ArrowRight') { this.focusedCarousel.next() }
+            if (e.key === 'ArrowLeft') {
+                this.focusedCarousel.previous();
+            } else if (e.key === 'ArrowRight') {
+                this.focusedCarousel.next();
+            }
         }
     },
     invoke: function () {
-        "use strict";
         return {
             initialize: this.initialize.bind(this),
-            carousels: this.carousels,
+            carousels: this.carousels
         };
     }
 }.invoke();
 
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
     window.osuny.carousel.manager.initialize();
 });
