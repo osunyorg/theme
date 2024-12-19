3eee559d883ef14fe5d8d938e030513b22b2f825
Alexis BENOIT <alex@noesya.coop>
2024-12-19 17:42:40 +0100

    add lightbox feature to handle video

--- assets/js/theme/blocks/videos.js
+++ assets/js/theme/blocks/videos.js
@@ -1,34 +1,50 @@
-const videos = document.querySelectorAll('.block-video');
+/* eslint-disable no-underscore-dangle */
+window.osuny = window.osuny || {};
+window.osuny.VideoPlayerFactory = window.osuny.VideoPlayerFactory || {};
+window.osuny.VideoPlayer = window.osuny.VideoPlayer || {};
 
-class VideoPlayer {
-    constructor (dom) {
-        this.dom = dom;
+window.osuny.VideoPlayer = function (element) {
+    this.element = element;
+    this.cover = this.element.querySelector('.lazy-video-player');
 
-        this.player = this.dom.querySelector('.lazy-video-player');
-
-        if (!this.player) {
-            return;
-        }
-
-        this.playBtn = this.player.querySelector('button');
-        this.videoIframe = this.dom.querySelector('.video-container iframe');
-        this.videoSrc = this.videoIframe.getAttribute('data-unloaded-src');
-
-        this.listen();
+    if (!this.cover) {
+        return;
     }
 
-    listen () {
-        this.playBtn.addEventListener('click', () => {
-            this.playVideo();
-        });
-    }
+    this.button = this.cover.querySelector('button');
+    this.iframe = this.element.querySelector('.video-container iframe');
+    this.iframeParent = this.iframe.parentElement;
+    this.src = this.iframe.getAttribute('data-unloaded-src');
 
-    playVideo () {
-        this.player.style.display = 'none';
-        this.videoIframe.src = this.videoSrc;
-    }
-}
+    this.listen();
+};
 
-videos.forEach((dom) => {
-    new VideoPlayer(dom);
-});
+window.osuny.VideoPlayer.prototype.listen = function () {
+    this.button.addEventListener('click', this.play.bind(this));
+};
+
+window.osuny.VideoPlayer.prototype.play = function () {
+    this.cover.style.display = 'none';
+    this.iframe.src = this.src;
+};
+
+window.osuny.VideoPlayer.prototype.cloneIframe = function () {
+    var clone = this.iframe.cloneNode();
+    clone.src = this.src;
+    return clone;
+};
+
+window.osuny.VideoPlayerFactory = function () {
+    var elements = document.querySelectorAll('.block-video');
+    this.videos = {};
+
+    elements.forEach(function (element) {
+        this.videos[element.id] = new window.osuny.VideoPlayer(element);
+    }.bind(this));
+};
+
+window.osuny.VideoPlayerFactory.prototype.get = function (id) {
+    return this.videos[id];
+};
+
+window.osuny.videoPlayerFactory = new window.osuny.VideoPlayerFactory();
