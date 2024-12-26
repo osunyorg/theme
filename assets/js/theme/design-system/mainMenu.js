399cc379c06d213d24b0a25a78a6a4c3c0b1370f
Alexis BENOIT <alex@noesya.coop>
2024-12-26 16:24:54 +0100

    styles

--- assets/js/theme/design-system/mainMenu.js
+++ assets/js/theme/design-system/mainMenu.js
@@ -76,8 +76,10 @@ class MainMenu {
     }
 
     resize () {
-        document.documentElement.style.setProperty('--header-height', this.element.offsetHeight + 'px');
-        document.documentElement.style.setProperty('--header-menu-max-height', window.innerHeight - this.element.offsetHeight + 'px');
+        if (!this.state.isOpened) {
+            document.documentElement.style.setProperty('--header-height', this.element.offsetHeight + 'px');
+            document.documentElement.style.setProperty('--header-menu-max-height', window.innerHeight - this.element.offsetHeight + 'px');
+        }
 
         // is state changed ?
         if (this.state.isMobile === isMobile()) {
