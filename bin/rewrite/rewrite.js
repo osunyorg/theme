0e6881751f58e9f2187e781f903772a5c0a40ca7
Alexis BENOIT <alex@noesya.coop>
2025-02-03 16:45:14 +0100

    cleanup

--- bin/rewrite/rewrite.js
+++ bin/rewrite/rewrite.js
@@ -1,15 +1,5 @@
-var data = require('./files'),
-    shell = require("shelljs");
-
-// module.exports = function () {
-//   data.forEach(function(replacement) {
-//     if (replacement.old_file && replacement.new_file) {
-//       console.log(`convert ${replacement.old_file} to ${replacement.new_file}`);
-//       shell.exec(`sh ./themes/osuny/bin/rewrite/replace.sh ${replacement.old_file} ${replacement.new_file}`);
-//     }
-//   });
-// }
+var shell = require("shelljs");
 
 module.exports = function () {
-  shell.exec(`sh ./themes/osuny/bin/rewrite/replace_2.sh`);
+  shell.exec(`sh ./themes/osuny/bin/rewrite/replace.sh`);
 }
