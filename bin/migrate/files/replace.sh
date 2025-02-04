47c0fff977b97c8081b7d7715ebaa214cf43d746
Alexis BENOIT <alex@noesya.coop>
2025-02-04 22:49:24 +0100

    fix data files and upgrade script

--- bin/migrate/files/replace.sh
+++ bin/migrate/files/replace.sh
@@ -6,6 +6,14 @@ files="themes/osuny/bin/migrate/files/v7-to-v8.csv"
 # Theme to update
 theme=$1
 
+# Vérifier la présence d'un dossier /layouts
+if [[ -d "./layouts" || -d "./themes/$theme/layouts" ]]; then
+    echo "Présence d'un dossier layouts"
+else
+    echo "Pas de dossiers layouts ni de thème à migrer"
+    exit 0
+fi
+
 # Ignorer la première ligne (les en-têtes) et gérer les lignes vides
 sed 1d "$files" | while IFS=',' read -r old new; do
     # Vérifier si la ligne est vide (après suppression des espaces)
@@ -17,37 +25,37 @@ sed 1d "$files" | while IFS=',' read -r old new; do
     old_path=$(echo "$old" | sed 's|themes/osuny/layouts/partials/||g')
     new_path=$(echo "$new" | sed 's|themes/osuny/layouts/partials/||g')
 
-    old_path=$(echo "$old_path" | sed 's|themes/osuny/layouts/||g')
-    new_path=$(echo "$new_path" | sed 's|themes/osuny/layouts/||g')
+    old_path_for_mv=$(echo "$old" | sed 's|themes/osuny/layouts/||g')
+    new_path_for_mv=$(echo "$new" | sed 's|themes/osuny/layouts/||g')
 
     # Traitement des données (affichage par exemple)
     echo "$old_path to $new_path"
+    echo "$old_path_for_mv to $new_path_for_mv"
 
-    # TODO : Ne chercher que dans les dossiers "layouts"
-    theme_path="*/themes/$theme/*"
-
-    find . \( -path "./layouts/*" -o -path "$theme_path" \) -type f -name "*.html" -exec sed -i '' "s/${old_path//\//\\/}/${new_path//\//\\/}/g" {} +
-
-    # S'il existe dans les overrides, déplacer le fichier vers le nouveau chemin
-    if [ -f "layouts/partials/$old_path" ]; then
-        mkdir -p "$(dirname "layouts/partials/$new_path")"
-        mv "layouts/partials/$old_path" "layouts/partials/$new_path"
+    find_in="./layouts/*"
+    replace_in="layouts"
+    if [ "$theme" != "" ]; then
+        find_in="./themes/$theme/*"
+        replace_in="themes/$theme/layouts"
     fi
 
-    if [ -f "layouts/$old_path" ]; then
-        mkdir -p "$(dirname "layouts/$new_path")"
-        mv "layouts/$old_path" "layouts/$new_path"
+    find . -path "$find_in" -type f -name "*.html" -exec sed -i '' "s/${old_path//\//\\/}/${new_path//\//\\/}/g" {} +
+
+    # S'il existe dans les overrides, déplacer le fichier vers le nouveau chemin
+    if [ -f "$replace_in/$old_path_for_mv" ]; then
+        mkdir -p "$(dirname "$replace_in/$new_path_for_mv")"
+        mv "$replace_in/$old_path_for_mv" "$replace_in/$new_path_for_mv"
     fi
 
     # S'il existe dans le thème custom, déplacer le fichier vers le nouveau chemin
-    if [ "$theme" != "" ];then
-        if [ -f "themes/$theme/layouts/partials/$old_path" ]; then
-            mkdir -p "$(dirname "themes/$theme/layouts/partials/$new_path")"
-            mv "themes/$theme/layouts/partials/$old_path" "themes/$theme/layouts/partials/$new_path"
-        fi
-        if [ -f "themes/$theme/layouts/$old_path" ]; then
-            mkdir -p "$(dirname "themes/$theme/layouts/$new_path")"
-            mv "themes/$theme/layouts/$old_path" "themes/$theme/layouts/$new_path"
-        fi
-    fi
+    # if [ "$theme" != "" ]; then
+    #     if [ -f "themes/$theme/layouts/partials/$old_path" ]; then
+    #         mkdir -p "$(dirname "themes/$theme/layouts/partials/$new_path")"
+    #         mv "themes/$theme/layouts/partials/$old_path" "themes/$theme/layouts/partials/$new_path"
+    #     fi
+    #     if [ -f "themes/$theme/layouts/$old_path" ]; then
+    #         mkdir -p "$(dirname "themes/$theme/layouts/$new_path")"
+    #         mv "themes/$theme/layouts/$old_path" "themes/$theme/layouts/$new_path"
+    #     fi
+    # fi
 done
