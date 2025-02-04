60ef8620f403dcb4c3b0bad96b6cc9122647dbcf
Alexis BENOIT <alex@noesya.coop>
2025-02-04 15:28:03 +0100

    script and data

--- bin/migrate/files/replace.sh
+++ bin/migrate/files/replace.sh
@@ -21,7 +21,7 @@ sed 1d "$files" | while IFS=',' read -r old new; do
     echo "$old_path to $new_path"
 
     # TODO : Ne chercher que dans les dossiers "layouts"
-    find . \( -path "*/layouts/*" -o -path "*/themes/*" \) -type f -name "*.html" -exec sed -i '' "s/${old_path//\//\\/}\"/${new_path//\//\\/}\"/g" {} +
+    find . \( -path "*/layouts/*" -o -path "*/themes/*" \) -type f -name "*.html" -exec sed -i '' "s/\"${old_path//\//\\/}/\"${new_path//\//\\/}/g" {} +
 
     # S'il existe dans les overrides, déplacer le fichier vers le nouveau chemin
     if [ -f "layouts/partials/$old_path" ]; then
@@ -31,7 +31,6 @@ sed 1d "$files" | while IFS=',' read -r old new; do
 
     # S'il existe dans le thème custom, déplacer le fichier vers le nouveau chemin
     if [ "$theme" != "" ];then
-        echo "$theme"
         if [ -f "themes/$theme/layouts/partials/$old_path" ]; then
             mkdir -p "$(dirname "themes/$theme/layouts/partials/$new_path")"
             mv "themes/$theme/layouts/partials/$old_path" "themes/$theme/layouts/partials/$new_path"
