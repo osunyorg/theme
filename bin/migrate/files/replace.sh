27865cbea5705607d2f58b0eb40932f5b6e2ce56
Arnaud Levy <arnaud.levy@noesya.coop>
2025-02-04 17:24:25 +0100

    Thème v8 (#910)
    Co-authored-by: Alexis BENOIT <alex@noesya.coop>

--- bin/migrate/files/replace.sh
+++ bin/migrate/files/replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Files to move
-files="themes/osuny/bin/migrate/files/files-to-move.csv"
+files="themes/osuny/bin/migrate/files/v7-to-v8.csv"
 
 # Theme to update
 theme=$1
@@ -17,11 +17,16 @@ sed 1d "$files" | while IFS=',' read -r old new; do
     old_path=$(echo "$old" | sed 's|themes/osuny/layouts/partials/||g')
     new_path=$(echo "$new" | sed 's|themes/osuny/layouts/partials/||g')
 
+    old_path=$(echo "$old_path" | sed 's|themes/osuny/layouts/||g')
+    new_path=$(echo "$new_path" | sed 's|themes/osuny/layouts/||g')
+
     # Traitement des données (affichage par exemple)
     echo "$old_path to $new_path"
 
     # TODO : Ne chercher que dans les dossiers "layouts"
-    find . \( -path "*/layouts/*" -o -path "*/themes/*" \) -type f -name "*.html" -exec sed -i '' "s/${old_path//\//\\/}\"/${new_path//\//\\/}\"/g" {} +
+    theme_path="*/themes/$theme/*"
+
+    find . \( -path "layouts/*" -o -path "$theme_path" \) -type f -name "*.html" -exec sed -i '' "s/${old_path//\//\\/}/${new_path//\//\\/}/g" {} +
 
     # S'il existe dans les overrides, déplacer le fichier vers le nouveau chemin
     if [ -f "layouts/partials/$old_path" ]; then
@@ -29,12 +34,20 @@ sed 1d "$files" | while IFS=',' read -r old new; do
         mv "layouts/partials/$old_path" "layouts/partials/$new_path"
     fi
 
+    if [ -f "layouts/$old_path" ]; then
+        mkdir -p "$(dirname "layouts/$new_path")"
+        mv "layouts/$old_path" "layouts/$new_path"
+    fi
+
     # S'il existe dans le thème custom, déplacer le fichier vers le nouveau chemin
     if [ "$theme" != "" ];then
-        echo "$theme"
         if [ -f "themes/$theme/layouts/partials/$old_path" ]; then
             mkdir -p "$(dirname "themes/$theme/layouts/partials/$new_path")"
             mv "themes/$theme/layouts/partials/$old_path" "themes/$theme/layouts/partials/$new_path"
+        fi
+        if [ -f "themes/$theme/layouts/$old_path" ]; then
+            mkdir -p "$(dirname "themes/$theme/layouts/$new_path")"
+            mv "themes/$theme/layouts/$old_path" "themes/$theme/layouts/$new_path"
         fi
     fi
 done
