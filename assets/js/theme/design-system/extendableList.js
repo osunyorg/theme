44831864376bb08b7dd475856d121d03569d8866
Alexis BENOIT <alex@noesya.coop>
2025-02-27 16:16:54 +0100

    fix js

--- assets/js/theme/design-system/extendableList.js
+++ assets/js/theme/design-system/extendableList.js
@@ -2,37 +2,43 @@ var osuny = window.osuny || {};
 osuny.ExtendableList = osuny.ExtendableList || {};
 
 osuny.ExtendableList = function (element) {
-    this.hiddenChildren = [];
-    this.hideClass = "truncated-list";
     osuny.Extendable.call(this, element);
-
+    this.hideClass = "truncated-list";
     this.element.classList.add(this.hideClass);
-    this.getHiddenChildren();
+    this.hiddenChildren = this.getHiddenChildren();
+    if (this.hiddenChildren.length === 0) {
+        this.deactivate();
+    }
 };
 
 osuny.ExtendableList.prototype = Object.create(osuny.Extendable.prototype);
 
-osuny.ExtendableList.prototype.getHiddenChildren = function (children) {
-    var children = this.element.querySelectorAll("li");
+osuny.ExtendableList.prototype.getHiddenChildren = function () {
+    var children = Array.prototype.slice.call(this.element.children),
+        hiddenChildren = [];
 
     children.forEach(function (child) {
         var style = window.getComputedStyle(child);
         if (style.getPropertyValue('display') === 'none') {
-            this.hiddenChildren.push(child);
+            hiddenChildren.push(child);
         }
-    }.bind(this));
+    });
 
-    if (this.hiddenChildren.length === 0) {
-        this.buttons.forEach(function (button) {
-            button.style.display = 'none';
-        });
-    }
+    return hiddenChildren;
 };
 
-osuny.ExtendableList.prototype.toggle = function (opened) {
-    osuny.Extendable.prototype.toggle.call(this, opened, true);
-    this.element.classList.toggle(this.hideClass);
-    this.focusFirstItem();
+osuny.ExtendableList.prototype.deactivate = function () {
+    this.buttons.forEach(function (button) {
+        button.style.display = 'none';
+    });
+}
+
+osuny.ExtendableList.prototype.toggle = function (opened, fromOutside) {
+    osuny.Extendable.prototype.toggle.call(this, opened, fromOutside);
+    if (this.state.opened) {
+        this.element.classList.remove(this.hideClass);
+        this.focusFirstItem();
+    }
 };
 
 osuny.ExtendableList.prototype.focusFirstItem = function (opened) {
