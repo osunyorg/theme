781b4906ff0e9b4862c8389aaa4c7466c1a8c982
Alexis BENOIT <alex@noesya.coop>
2025-03-10 18:35:52 +0100

    sticky titles

--- assets/js/theme/design-system/StickyTitle.js
+++ assets/js/theme/design-system/StickyTitle.js
@@ -11,15 +11,22 @@ osuny.StickyTitle = function (container, titleSelector) {
 
 osuny.StickyTitle.prototype.listen = function () {
     var distance = 0,
-        threshold = 10;
+        threshold = 10,
+        hours = this.container.querySelector('.event:last-child .event-hours span');
     window.addEventListener('scroll', function() {
         distance = Math.abs(this.title.offsetTop - this.container.offsetTop)
         if (distance > threshold) {
             this.title.classList.add('is-down');
+            this.container.style.maxHeight = this.container.offsetHeight + "px";
         } else {
             this.title.classList.remove('is-down');
+            this.container.style.maxHeight = "none";
         }
         this.container.style.setProperty('--title-height', this.title.offsetHeight + 'px');
+
+        if (hours) {
+            this.container.style.setProperty('--title-margin-bottom', hours.offsetHeight + 'px');
+        }
     }.bind(this));
 };
 
