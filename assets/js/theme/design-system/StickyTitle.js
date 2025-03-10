9d3d174283a8cd9f04dc140f868c8f90745a4622
Alexis BENOIT <alex@noesya.coop>
2025-03-10 18:49:21 +0100

    sticky title wip

--- assets/js/theme/design-system/StickyTitle.js
+++ assets/js/theme/design-system/StickyTitle.js
@@ -12,17 +12,21 @@ osuny.StickyTitle = function (container, titleSelector) {
 osuny.StickyTitle.prototype.listen = function () {
     var distance = 0,
         threshold = 10,
+        titleOriginalHeight = this.title.offsetHeight,
+        titleDownHeight = this.title.querySelector('span').offsetHeight,
         hours = this.container.querySelector('.event:last-child .event-hours span');
+    console.log(titleDownHeight)
     window.addEventListener('scroll', function() {
-        distance = Math.abs(this.title.offsetTop - this.container.offsetTop)
+        distance = Math.abs(this.title.offsetTop - this.container.offsetTop);
+
         if (distance > threshold) {
             this.title.classList.add('is-down');
-            this.container.style.maxHeight = this.container.offsetHeight + "px";
+            titleDownHeight = this.title.querySelector('span').offsetHeight;
         } else {
             this.title.classList.remove('is-down');
-            this.container.style.maxHeight = "none";
         }
-        this.container.style.setProperty('--title-height', this.title.offsetHeight + 'px');
+        this.title.style.maxHeight = titleOriginalHeight + "px";
+        this.container.style.setProperty('--title-height', titleDownHeight + 'px');
 
         if (hours) {
             this.container.style.setProperty('--title-margin-bottom', hours.offsetHeight + 'px');
