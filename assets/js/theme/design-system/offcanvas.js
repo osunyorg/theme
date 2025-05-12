b955f684d9d3af4c6538824884dfc5f1e1f590d1
Alexis BENOIT <alex@noesya.coop>
2025-05-12 18:46:51 +0200

    style

--- assets/js/theme/design-system/offcanvas.js
+++ assets/js/theme/design-system/offcanvas.js
@@ -3,15 +3,23 @@ osuny.Offcanvas = osuny.Offcanvas || {};
 
 osuny.Offcanvas = function (element) {
     osuny.Extendable.call(this, element);
+    this.classes = {
+        bodyclass: 'has-offcanvas-opened',
+        opened: 'is-opened'
+    }
 };
 
 osuny.Offcanvas.prototype = Object.create(osuny.Extendable.prototype);
 
 osuny.Offcanvas.prototype.toggle = function (opened, fromOutside) {
+    var classAction;
     osuny.Extendable.prototype.toggle.call(this, opened, fromOutside);
-    this.element.classList[this.state.opened ? 'add' : 'remove']('is-opened');
-};
 
+    classAction = this.state.opened ? 'add' : 'remove';
+
+    this.element.classList[classAction](this.classes.opened);
+    document.documentElement.classList[classAction](this.classes.bodyclass);
+};
 
 (function () {
     osuny.utils.instanciateAll('.offcanvas', osuny.Offcanvas);
