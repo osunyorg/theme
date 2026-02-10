57a6fe654b391db6825c0f79d518c862e3c935a5
Arnaud Levy <contact@arnaudlevy.com>
2026-02-10 19:20:28 +0100

    complexity

---
+++ bin/analyze.rb
@@ -0,0 +1,44 @@
+KEYWORDS = [
+  'if',
+  'else',
+  'for',
+  'range',
+  'with',
+].freeze
+
+DANGER = 10
+WARNING = 5
+
+files = []
+
+def occurences(needle, haystack)
+  return 0 if haystack.empty?
+  haystack.split(needle).count - 1
+end
+
+def compute_complexity(data)
+  complexity = 1
+  KEYWORDS.each do |keyword|
+    complexity += occurences("#{keyword} ", data)
+  end
+  complexity
+end
+
+Dir.glob('./layouts/**/*').each do |path|
+  next if File.directory?(path)
+  data = File.read(path)
+  complexity = compute_complexity(data)
+  files << {
+    path: path,
+    complexity: complexity
+  }
+end
+
+files.sort_by! { |hash| hash[:complexity] }.reverse!
+
+puts "| File | Complexity | State |"
+files.each do |hash|
+  next if hash[:complexity] < WARNING
+  icon = hash[:complexity] > DANGER ? '❌' : '⚠️'
+  puts "| #{icon} | #{hash[:complexity]} | #{hash[:path]} |"
+end
