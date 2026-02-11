b58a95cd4ebbd2558cc48b5040c185fca988a628
Arnaud Levy <contact@arnaudlevy.com>
2026-02-11 09:41:37 +0100

    test

--- bin/analyze.rb
+++ bin/analyze.rb
@@ -9,6 +9,7 @@ KEYWORDS = [
 DANGER = 10
 WARNING = 5
 
+paths = Dir.glob('./layouts/**/*').reject { |path| File.directory?(path) }
 files = []
 
 def occurences(needle, haystack)
@@ -16,32 +17,81 @@ def occurences(needle, haystack)
   haystack.split(needle).count - 1
 end
 
+def occurrences_in_files(needle, files)
+  occurences = 0
+  files.each do |file|
+    occurences += occurences(needle, file[:data])
+  end
+  occurences
+end
+
 def compute_complexity(data)
   complexity = 1
   KEYWORDS.each do |keyword|
     complexity += occurences("#{keyword} ", data)
   end
-  complexity
-end
-
-Dir.glob('./layouts/**/*').each do |path|
-  next if File.directory?(path)
-  data = File.read(path)
-  complexity = compute_complexity(data)
-  files << {
-    path: path,
-    complexity: complexity
+  icon = '✅'
+  if complexity > DANGER
+    icon = '❌'
+  elsif complexity > WARNING
+    icon = '⚠️'
+  end
+  {
+    score: complexity,
+    icon: icon,
+    problem: complexity > WARNING
   }
 end
 
-files.sort_by! { |hash| hash[:complexity] }.reverse!
+def count_calls(path, files)
+  root = './layouts/partials/'
+  if path.include?(root)
+    fragment = path.gsub(root, '').gsub('.html', '')
+    call = "partial \"#{fragment}"
+    count = occurrences_in_files(call, files)
+    {
+      fragment: fragment,
+      count: count
+    }
+  else
+    nil
+  end
+end
 
+# Load content
+paths.each do |path|
+  next if File.directory?(path)
+  data = File.read(path)
+  files << {
+    path: path,
+    short_path: path.gsub('./layouts/', ''),
+    data: data
+  }
+end
+
+# Analyze
+files.each do |file|
+  complexity = compute_complexity(file[:data])
+  file[:complexity] = complexity
+  file[:calls] = count_calls(file[:path], files)
+end
+
+puts "## Complexity"
 puts "| State | Complexity | File |"
 puts "|---|---|---|"
-files.each do |hash|
-  next if hash[:complexity] < WARNING
-  icon = hash[:complexity] > DANGER ? '❌' : '⚠️'
-  path = hash[:path].gsub('./layouts/', '')
-  complexity = hash[:complexity]
-  puts "| #{icon} | #{complexity} | #{path} |"
+files.sort_by{ |hash| hash[:complexity][:score] }
+     .reverse
+     .each do |file|
+  next unless file[:complexity][:problem]
+  puts "| #{file[:complexity][:icon]} | #{file[:complexity][:score]} | #{file[:short_path]} |"
+end
+
+puts "## Partials calls"
+puts "| Calls | Partial |"
+puts "|---|---|"
+files.reject { |hash| hash[:calls].nil? }
+     .sort_by { |hash| hash[:calls][:count] }
+     .reverse
+     .each do |file|
+  puts "| #{file[:calls][:count]} | #{file[:calls][:fragment]} |"
 end
