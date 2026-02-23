5167bcf21ad99b7ba06332891809147c5b0e41f1
Alexis BENOIT <alex@noesya.coop>
2026-02-23 13:20:46 +0100

    add rvw unit to handle windows OS real viewport width

--- assets/js/theme/utils/realViewportHeight.js
+++
@@ -1,8 +0,0 @@
-var setRealViewportHeight = function () {
-    var rvh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--rvh', rvh + 'px');
-};
-
-window.addEventListener('load', setRealViewportHeight);
-window.addEventListener('resize', setRealViewportHeight);
-
