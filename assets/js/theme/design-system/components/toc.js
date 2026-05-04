e2e3c0a065f072aa221bec41e857527e8d9dcb8e
Olivia Simonet <91660674+Olivia206@users.noreply.github.com>
2026-05-04 11:02:17 +0200

    [A11Y] Gestion du focus de la toc (#1510)
    Co-authored-by: Arnaud Levy <contact@arnaudlevy.com>

--- assets/js/theme/design-system/components/toc.js
+++ assets/js/theme/design-system/components/toc.js
@@ -3,127 +3,68 @@ import { focusTrap } from '../../utils/focus-trap';
 
 window.osuny = window.osuny || {};
 
-var CLASSES = {
-    offcanvasOpened: 'has-offcanvas-opened',
-    linkActive: 'active',
-    isOpened: 'is-opened',
-    fullWidth: 'full-width',
-    offcanvas: 'offcanvas-toc'
-};
-
 window.osuny.TableOfContents = function (element) {
-    this.element = element;
-    this.content = this.element.querySelector('.toc-content');
-    this.nav = this.element.querySelector('.toc');
-    this.links = this.element.querySelectorAll('a');
-    this.sections = this.getSections();
-    this.ctaTitle = document.querySelector('.toc-cta-title span');
-    this.openerButton = document.querySelector('.toc-cta button');
-    this.togglers = document.querySelectorAll('.toc-cta button, .toc-container button');
+    this.elements = {
+        root: element,
+        content: element.querySelector('.toc-content'),
+        nav: element.querySelector('.toc'),
+        links: element.querySelectorAll('a'),
+        sections: [],
+        buttonOpen: document.querySelector('.toc-cta button'),
+        buttonClose: document.querySelector('.toc-content > button')
+    }
+
     this.state = {
         opened: false,
         currentId: null,
         currentLink: 0,
         isOffcanvas: null
     };
+
+    this.classes = {
+        offcanvasOpened: 'has-offcanvas-opened',
+        linkActive: 'active',
+        isOpened: 'is-opened',
+        fullWidth: 'full-width',
+        offcanvas: 'offcanvas-toc'
+    };
+
+    this.initializeSections();
+    this.initializeAria();
     this.listen();
-    
-    this.state.isOffcanvas = this.isOffcanvas()
-    if (this.state.isOffcanvas) {
-        this.element.setAttribute('aria-hidden', true);
-    }
 };
 
-window.osuny.TableOfContents.prototype.getSections = function () {
-    var sections = [],
-        id,
-        section;
+// Initialisation
 
-    this.links.forEach(function (link) {
+window.osuny.TableOfContents.prototype.initializeSections = function () {
+    var id,
+        section,
+        sections = this.elements.sections;
+    this.elements.links.forEach( function (link) {
         id = link.getAttribute('href').replace('#', '');
         section = document.getElementById(id);
         if (section) {
             sections.push(section);
         }
     });
-
-    return sections;
 };
+
+window.osuny.TableOfContents.prototype.initializeAria = function () {
+    this.state.isOffcanvas = this.isOffcanvas()
+    if (this.state.isOffcanvas) {
+        this.elements.root.setAttribute('aria-hidden', 'true');
+    }
+}
 
 window.osuny.TableOfContents.prototype.isOffcanvas = function () {
-    return isMobile() || document.body.classList.contains(CLASSES.fullWidth) || document.body.classList.contains(CLASSES.offcanvas);
-};
-
-window.osuny.TableOfContents.prototype.listen = function () {
-    window.addEventListener('scroll', this.update.bind(this), false);
-
-    window.addEventListener('resize', this.resize.bind(this), false);
-
-    this.togglers.forEach( function (toggler) {
-        toggler.addEventListener('click', function () {
-            this.toggle();
-        }.bind(this));
-    }.bind(this));
-
-    this.links.forEach( function (links) {
-        links.addEventListener('click', function () {
-            this.toggle(false);
-        }.bind(this));
-    }.bind(this));
-
-    window.addEventListener('click', function (event) {
-        if (event.target === document.body) {
-            this.toggle(false);
-        }
-    }.bind(this));
-
-    window.addEventListener('keydown', function (event) {
-        if (event.keyCode === 27 || event.key === 'Escape') {
-            this.toggle(false);
-        }
-        focusTrap(event, this.element, this.state.opened);
-    }.bind(this));
-};
-
-window.osuny.TableOfContents.prototype.toggle = function (open) {
-    if (!this.state.isOffcanvas) {
-        return;
-    }
-    this.state.opened = typeof open !== 'undefined' ? open : !this.state.opened;
-    var classAction = this.state.opened ? 'add' : 'remove',
-        transitionDuration = this.state.opened ? 0 : this.getTransitionDuration();
-
-    // TODO: refacto timeout and css transition
-    setTimeout( function () {
-        this.element.setAttribute('aria-hidden', !this.state.opened);
-    }.bind(this), transitionDuration * 1000);
-
-    setTimeout( function () {
-        this.element.classList[classAction](CLASSES.isOpened);
-    }.bind(this), 50);
-
-    document.documentElement.classList[classAction](CLASSES.offcanvasOpened);
+    return isMobile() || document.body.classList.contains(this.classes.fullWidth) || document.body.classList.contains(this.classes.offcanvas);
 };
 
 window.osuny.TableOfContents.prototype.getTransitionDuration = function () {
-    var transitionDuration = getComputedStyle(this.element).getPropertyValue('--toc-transition-duration');
-    transitionDuration = parseFloat(transitionDuration.replace('s', ''))
-    return transitionDuration;
-};
-
-window.osuny.TableOfContents.prototype.update = function () {
-    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    var id = null;
-    this.sections.forEach(function (section) {
-        if (this.getAbsoluteOffsetTop(section) <= scroll + window.innerHeight / 2) {
-            id = section.id;
-        }
-    }.bind(this));
-
-    if (id && id !== this.state.currentId) {
-        this.activateLink(id);
-    }
-    this.updateScrollspy(scroll);
+    var style = getComputedStyle(this.elements.root),
+        property = style.getPropertyValue('transition-duration'),
+        milliseconds = parseFloat(property.replace('s', '')) * 1000;
+    return milliseconds;
 };
 
 window.osuny.TableOfContents.prototype.getAbsoluteOffsetTop = function (element) {
@@ -135,34 +76,91 @@ window.osuny.TableOfContents.prototype.getAbsoluteOffsetTop = function (element)
     return top;
 };
 
-window.osuny.TableOfContents.prototype.activateLink = function (id) {
-    var currentLink = this.element.querySelector(`[href*='${id}']`);
+window.osuny.TableOfContents.prototype.listen = function () {
+    window.addEventListener('scroll', this.update.bind(this));
+    window.addEventListener('resize', this.resize.bind(this));
+    window.addEventListener('click', this.clickOnBackground.bind(this));
+    window.addEventListener('keydown', this.keyDown.bind(this));
 
-    this.links.forEach( function (link) {
+    if (this.elements.buttonOpen) {
+        this.elements.buttonOpen.addEventListener('click', this.open.bind(this));
+    }
+    if (this.elements.buttonClose) {
+        this.elements.buttonClose.addEventListener('click', this.closeAndFocusButtonOpen.bind(this));
+    }
+    this.elements.links.forEach( function (link) {
+        link.addEventListener('click', this.close.bind(this));
+    }.bind(this));
+};
+
+// Méthodes
+
+window.osuny.TableOfContents.prototype.open = function () {
+    if (!this.state.isOffcanvas) {
+        return;
+    }
+    this.state.opened = true;
+    this.elements.root.setAttribute('aria-hidden', 'false');
+    document.documentElement.classList.add(this.classes.offcanvasOpened);
+    setTimeout( function () {
+        // Pour permettre l'animation d'apparition
+        this.elements.root.classList.add(this.classes.isOpened);
+        // Pour éviter de donner le focus à un élément caché
+        this.elements.buttonClose.focus();
+    }.bind(this), 50);
+};
+
+window.osuny.TableOfContents.prototype.close = function () {
+    if (!this.state.isOffcanvas) {
+        return;
+    }
+    this.state.opened = false;
+    this.elements.root.classList.remove(this.classes.isOpened);
+    document.documentElement.classList.remove(this.classes.offcanvasOpened);
+    setTimeout( function () {
+        this.elements.root.setAttribute('aria-hidden', 'true');
+    }.bind(this), this.getTransitionDuration());
+};
+
+// focus opener button, see: https://github.com/osunyorg/theme/issues/1015
+window.osuny.TableOfContents.prototype.closeAndFocusButtonOpen = function () {
+    this.close();
+    this.elements.buttonOpen.focus();
+};
+
+window.osuny.TableOfContents.prototype.activateLink = function (id) {
+    var currentLink = this.elements.root.querySelector(`[href*='${id}']`);
+
+    this.elements.links.forEach( function (link) {
         if (link === currentLink) {
-            link.classList.add(CLASSES.linkActive);
+            link.classList.add(this.classes.linkActive);
             link.setAttribute('aria-current', 'true');
-            this.updateCtaTitle(link);
-            this.state.id = id;
+            this.state.currentId = id;
             this.state.currentLink = link;
         } else {
-            link.classList.remove(CLASSES.linkActive);
+            link.classList.remove(this.classes.linkActive);
             link.removeAttribute('aria-current');
         }
     }.bind(this));
 };
 
-window.osuny.TableOfContents.prototype.updateCtaTitle = function (link) {
-    if (isMobile()) {
-        this.openerButton.setAttribute('aria-label', link.innerText +  osuny.i18n.toc.button_label);
-        this.ctaTitle.innerText = link.innerText;
-    } else {
-        this.ctaTitle.innerText = this.ctaTitle.getAttribute('data-default');
+window.osuny.TableOfContents.prototype.update = function () {
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    var id = null;
+    this.elements.sections.forEach(function (section) {
+        if (this.getAbsoluteOffsetTop(section) <= scroll + window.innerHeight / 2) {
+            id = section.id;
+        }
+    }.bind(this));
+
+    if (id && id !== this.state.currentId) {
+        this.activateLink(id);
     }
+    this.updateScrollspy(scroll);
 };
 
 window.osuny.TableOfContents.prototype.updateScrollspy = function (scroll) {
-    var container = this.state.isOffcanvas ? this.nav : this.content;
+    var container = this.state.isOffcanvas ? this.elements.nav : this.elements.content;
     if (this.state.currentLink && scroll > window.innerHeight) {
         var progress = this.getAbsoluteOffsetTop(this.state.currentLink) - container.offsetHeight / 2;
         progress = this.state.isOffcanvas ? progress : progress - scroll;
@@ -174,6 +172,25 @@ window.osuny.TableOfContents.prototype.updateScrollspy = function (scroll) {
 
 window.osuny.TableOfContents.prototype.resize = function () {
     this.state.isOffcanvas = this.isOffcanvas();
+};
+
+window.osuny.TableOfContents.prototype.clickOnBackground = function (event) {
+    if (!this.state.opened) {
+        return;
+    }
+    if (event.target === document.body) {
+        this.closeAndFocusButtonOpen();
+    }
+};
+
+window.osuny.TableOfContents.prototype.keyDown = function (event) {
+    if (!this.state.opened) {
+        return;
+    }
+    if (event.keyCode === 27 || event.key === 'Escape') {
+        this.closeAndFocusButtonOpen();
+    }
+    focusTrap(event, this.elements.root, true);
 };
 
 window.osuny.page.registerComponent({
